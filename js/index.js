9cb68bc3bdfbd4ed35fe60ee200938f6490f66aa
Your Name <you@example.com>
2019-03-05 18:46:11 -0500

    added more events to page elements

--- js/index.js
+++ js/index.js
@@ -1,58 +1,143 @@
-
+//opacity for nav bar
 const navBar = document.querySelector(".main-navigation");
-window.addEventListener("scroll", function() {
+navBar.addEventListener("scroll", function() {
 
-if (window.pageYOffset > 0) {
-    navBar.style.opacity = ".8";
+if (navBar.position > 0 ) {
+navBar.style.opacity = ".5";
 }
 });
 
+//change header image on click 
+const headImg = document.querySelector("img");
+headImg.addEventListener('mouseover', function(e){
+headImg.style.borderRadius = "50px";
+e.stopPropagation();
+
+})
+
+headImg.addEventListener('mouseleave', function(e){
+    headImg.style.borderRadius = "0";
+    e.stopPropagation();
+    
+    })
+
+
+//opacity for mouseover images
 const imgOpacity = document.querySelectorAll('.img-content');
 
 const newImgContent = Array.from(imgOpacity);
 
-for(let i = 0; i< newImgContent.length; i++){
-newImgContent[i].addEventListener("mouseover", function(e) {
-    newImgContent[i].style.opacity = ".2";
+
+newImgContent.forEach((currentValue) => {
+currentValue.addEventListener('mouseover', function(e){
+    currentValue.style.opacity = ".2";
     e.stopPropagation();
+})
 });
-}
 
-for(let i = 0; i< newImgContent.length; i++){
-    newImgContent[i].addEventListener("mouseleave", function(e) {
-        newImgContent[i].style.opacity = "1";
-        e.stopPropagation();
+newImgContent.forEach((currentValue) => {
+currentValue.addEventListener('mouseleave', function(e){
+    currentValue.style.opacity = "1";
+    e.stopPropagation();
+})
 });
-}
 
-
-const navigator = document.querySelectorAll('.nav-link');
+//nav link color change on mouseover
+const navigator = document.querySelectorAll('a');
 const navLinks = Array.from(navigator);
 console.log(navLinks);
 
-for(let i = 0; i< navLinks.length; i++){
-navLinks[i].addEventListener("mouseover", function(e) {
-    navLinks[i].style.scale = "2";
+navLinks.forEach((currentValue) => {
+currentValue.addEventListener('mouseover', function(e){
+    currentValue.style.color = "gray";
     e.stopPropagation();
+    e.preventDefault();
+    
+})
 });
-}
-for(let i = 0; i< navLinks.length; i++){
-    navLinks[i].addEventListener("mouseleave", function() {
-        navLinks[i].style.scale = "1";
-    });
-    }
 
+navLinks.forEach((currentValue) => {
+currentValue.addEventListener('mouseleave', function(e){
+    currentValue.style.color = "black";
+    e.stopPropagation();
+})
+});
+
+
+//color and size change on click
+navLinks.forEach((currentValue) => {
+currentValue.addEventListener('mousedown', function(e){
+    currentValue.style.color = "#ffd800";
+    currentValue.style.fontSize = "2rem";
+    e.preventDefault();
+})
+})
+
+navLinks.forEach((currentValue) => {
+currentValue.addEventListener('mouseup', function(e){
+    currentValue.style.color = "black";
+    currentValue.style.fontSize = "16px";
+    e.preventDefault();
+})
+})
+
+
+
+
+//text underline for h2 and h4 on mouseover
 const headings = document.querySelectorAll ('h2, h4');
 const newHeadings = Array.from (headings);
 
-for(let i = 0; i< newHeadings.length; i++){
-newHeadings[i].addEventListener("mouseover", function() {
-    newHeadings[i].style.textDecoration = "underline";
+newHeadings.forEach((currentValue) => {
+currentValue.addEventListener('mouseover', function(event){
+    currentValue.style.textDecoration = "underline"
+    event.preventDefault();
+})
+})
+
+newHeadings.forEach((currentValue) => {
+currentValue.addEventListener('mouseleave', function(event){
+    currentValue.style.textDecoration = "none"
+    event.preventDefault();
+})
+})
+
+
+
+
+//sound for mouse down 
+const button = document.querySelectorAll('.btn');
+const arrBtn = Array.from(button);
+
+
+
+arrBtn.forEach((currentValue) => {
+let sound = document.querySelector('.audio');
+sound.setAttribute('src', "img/flock-of-seagulls_daniel-simion.wav");
+currentValue.addEventListener('mousedown', function(){
+    sound.play();
+})
+
 });
-}
-for(let i = 0; i< newHeadings.length; i++){
-    newHeadings[i].addEventListener("mouseleave", function() {
-        newHeadings[i].style.textDecoration = "none";
-    });
-    }
+
+const sound2 = document.querySelector('.audio');
+sound2.setAttribute('src', "img/yodel.mp3");
+const mtnBtn = arrBtn[1];
+mtnBtn.addEventListener('mousedown', function(){
+sound2.play(); 
+
+
+})
+
+
+arrBtn.forEach((currentValue) => {
+let sound = document.querySelector('.audio');
+sound.setAttribute('src', "img/flock-of-seagulls_daniel-simion.wav");
+currentValue.addEventListener('mouseup', function(){
+sound.pause();
+sound.currentTime = 0;
+})
+});
+
+
 
